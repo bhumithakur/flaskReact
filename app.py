aa96bff72dae9b0588384e6e70763c3a7f38da3f
Vipin Kumar <58135705+devVipin01@users.noreply.github.com>
2023-04-25 17:08:49 +0530

    app_update

--- app.py
+++ app.py
@@ -6,40 +6,46 @@ import tensorflow as tf
 
 model = tf.keras.models.load_model("breast_model.h5")
 #model.load_weights('mymodel.h5')
-print("model load done")
+
+
 app = Flask(__name__)
 
-# Define a route for your homepage
-@app.route('/', methods=['GET'])
-def hello_world():
-    return {"members": ["mem1","mem2"]}
+dic={0 :"IDC Negative", 1 : "IDC Positive"}
 
-# Define a route for uploading an image
-@app.route('/predict', methods=['POST'])
-def predict():
-    # Get the uploaded image from the request
-    imagefile = request.files['imagefile']
-    # Save the image to a file on your server
-    image_path = './images/' + imagefile.filename
-    imagefile.save(image_path)
-    # Render the app.js file to display the uploaded image
-
-    img=Image.open(image_path).convert('RGB')
+#prediction
+def predict_label(path):
+    img=Image.open(path).convert('RGB')
     img=img.resize((50,50),resample=Image.BILINEAR)
     img=np.array(img,dtype=np.float32)/255.0
     test_img=np.expand_dims(img,axis=0)
 
     pre=model.predict(test_img)
-    pre_class=np.argmax(pre)
+    return dic[np.argmax(pre)]
+    #pre_class=np.argmax(pre)
 
-    if pre_class==1:
-        print("IDC Positive")
-    else:
-        print("IDC Negative")
-    #prediction
-    pre=model.predict(np.expend_dims(img,axis=0))[0]
-    print("predict result : ",pre)
-    return render_template('app.js', image_path=pre)#image_path=image_path)
+    
+# Define a route for your homepage
+@app.route('/', methods=['GET'])
+def hello_world():
+    return {"members": ["mem1","mem2"]}
+
+
+# Define a route for uploading an image
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        img = request.files['my_image']
+        img_path = "static/" + img.filename
+        img.save(img_path)
+        pred = predict_label(img_path)
+        
+
+##    if pre_class==1:
+##        print("IDC Positive")
+##    else:
+##        print("IDC Negative")
+        
+    return render_template('app.js',prediction = pred,image_path=img_path)
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
